034ab3548cf07ce0f745c737defc934daa84f1f6
France Gelasque <fgelasque93@gmail.com>
2017-02-05 23:54:58 +0800

    Firebase Setup and Integration to facebook login

--- app/src/main/java/app/twinstartech/travlr/activities/LoginActivity.java
+++ app/src/main/java/app/twinstartech/travlr/activities/LoginActivity.java
@@ -2,15 +2,29 @@ package app.twinstartech.travlr.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
@@ -28,21 +42,51 @@ public class LoginActivity extends TravlrActivity {
     Button btnLogin;
 
     CallbackManager callbackManager;
+
+
+    /**
+     * FIREBASE VARIABLES
+     *
+     */
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+
+        /**
+         * FIREBASE SETUP
+         */
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
+
+
+        /**
+         * FACEBOOK SETUP
+         */
         callbackManager = CallbackManager.Factory.create();
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.e(TAG,loginResult.getAccessToken().getToken());
+                handleFacebookAccessToken(loginResult.getAccessToken());
 
-                Constants.currentProfile = Profile.getCurrentProfile();
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                finish();
             }
 
             @Override
@@ -52,15 +96,38 @@ public class LoginActivity extends TravlrActivity {
 
             @Override
             public void onError(FacebookException error) {
-
                 Log.e(TAG,error.getLocalizedMessage());
             }
         });
+
+
+        /**
+         *
+         * Check if facebook already logged in
+         *
+         */
+
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if(token != null ){
+            if(!TextUtils.isEmpty(token.getToken())) {
+                Log.e(TAG, "Already loggedin!");
+//                LoginManager.getInstance().logOut();
+                handleFacebookAccessToken(AccessToken.getCurrentAccessToken());
+            }
+            else  {
+                btnLogin.setVisibility(View.VISIBLE);
+            }
+        } else  {
+            btnLogin.setVisibility(View.VISIBLE);
+        }
     }
 
+    /**
+     * START FACEBOOK LOGIN
+     */
     @OnClick(R.id.btnLogin)
     void login(){
-        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile"));
+        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile","email"));
     }
 
     @Override
@@ -68,4 +135,52 @@ public class LoginActivity extends TravlrActivity {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode,resultCode,data);
     }
+
+    /***
+     *
+     */
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,
+                                    "Welcome back "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            finish();
+                        }
+                    }
+                });
+    }
+
 }
